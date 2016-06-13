b6291ef4af31327d92e8cf4551e83b3d69586e6c
pierrickftw <pierrickftw@gmail.com>
2016-06-13 16:43:38 +0200

    16h45
    Changements mineurs

--- connexion.php
+++ connexion.php
@@ -19,11 +19,11 @@
     		// on redirige notre visiteur vers une page de notre section membre
     		header ('location: ./home_page.php');
     	}else {
-			header('location: ./Inscription_Validation.html');	
+			header('location: ./Connexion_Erreur.html');	
 		// Le visiteur n'a pas été reconnu comme étant membre de notre site. On utilise alors un petit javascript lui signalant ce fait
     		echo '<body onLoad="alert(\'Nom ou mot de passe incorrect\')">';
     		// puis on le redirige vers la page d'accueil
-    		echo '<meta http-equiv="refresh" content="0;URL=autobat.php">';
+    		echo '<meta http-equiv="refresh" content="0;URL=home_page.php">';
     	}
 }
     else {
